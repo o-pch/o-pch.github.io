56fe1095820c77a9d0704cba53b73d6d4f91a210
oponkichi <o.ponkichi@gmail.com>
2026-01-11 17:25:24 +0900

    MobileブラウザでのVirtual Keypad追加

--- src/components/GameBoyEmulator.jsx
+++ src/components/GameBoyEmulator.jsx
@@ -21,12 +21,144 @@ export default function GameBoyEmulator({ romPath = 'files/myrpg_demo_release.gb
     // Set global EmulatorJS variables
     window.EJS_player = '#gameboy-container'
     window.EJS_core = 'gb'
-    window.EJS_gameUrl = romPath
+    window.EJS_gameName = romPath.split('/').pop()
+    window.EJS_gameUrl = absoluteRomUrl
     window.EJS_biosUrl = ''
     window.EJS_pathtodata = 'https://cdn.emulatorjs.org/stable/data/'
     // Use Japanese language (ja-JA.json is available in CDN)
     window.EJS_language = 'ja-JA'
     window.EJS_startOnLoaded = true
+    window.EJS_AdTimer = -1
+
+    window.EJS_Buttons = {
+      playPause: false,
+      restart: false,
+      mute: false,
+      settings: false,
+      fullscreen: true,
+      saveState: false,
+      loadState: false,
+      screenRecord: false,
+      gamepad: false,
+      cheat: false,
+      volume: true,
+      saveSavFiles: true,
+      loadSavFiles: true,
+      quickSave: false,
+      quickLoad: false,
+      screenshot: false,
+      cacheManager: false,
+      exitEmulation: false
+    }
+
+    window.EJS_VirtualGamepadSettings = [
+    {
+        type: "button",
+        text: "A",
+        id: "a",
+        location: "right",
+        left: 81,
+        top: 120,
+        bold: true,
+        input_value: 8
+    },
+    {
+        type: "button",
+        text: "B",
+        id: "b",
+        location: "right",
+        left: 20,
+        top: 120,
+        bold: true,
+        input_value: 0
+    },
+    //Note- the dpad and the zone will overlap in this example, this is just to show what it should look like.
+    {
+        type: "dpad",
+        location: "left",
+        left: "50%",
+        right: "50%",
+        joystickInput: false,
+        inputValues: [4, 5, 6, 7]
+    },
+    {
+        type: "button",
+        text: "Start",
+        id: "start",
+        location: "right",
+        left: 80,
+        fontSize: 15,
+        block: true,
+        input_value: 3
+    },
+    {
+        type: "button",
+        text: "Select",
+        id: "select",
+        location: "right",
+        left: 20,
+        fontSize: 15,
+        block: true,
+        input_value: 2
+    }
+]
+
+    // Check if device is mobile
+    const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+    
+    // Only set VirtualGamepadSettings for mobile devices
+    if (isMobile) {
+      window.EJS_VirtualGamepadSettings = [
+        {
+            type: "button",
+            text: "A",
+            id: "a",
+            location: "right",
+            left: 81,
+            top: 120,
+            bold: true,
+            input_value: 8
+        },
+        {
+            type: "button",
+            text: "B",
+            id: "b",
+            location: "right",
+            left: 20,
+            top: 120,
+            bold: true,
+            input_value: 0
+        },
+        {
+            type: "dpad",
+            location: "left",
+            left: "50%",
+            right: "50%",
+            joystickInput: false,
+            inputValues: [4, 5, 6, 7]
+        },
+        {
+            type: "button",
+            text: "Start",
+            id: "start",
+            location: "right",
+            left: 80,
+            fontSize: 15,
+            block: true,
+            input_value: 3
+        },
+        {
+            type: "button",
+            text: "Select",
+            id: "select",
+            location: "right",
+            left: 20,
+            fontSize: 15,
+            block: true,
+            input_value: 2
+        }
+      ]
+    }
 
     // Create a unique ID for the loader script to avoid duplicates
     const loaderId = 'emulatorjs-loader'
